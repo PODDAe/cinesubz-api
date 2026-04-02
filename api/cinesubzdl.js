3b76c92547e8eec99696d84e1bb9683401981fac
PODDAe <camalkaakash2@gmail.com>
2026-04-03 01:25:45 +0530

    Update cinesubzdl.js

--- api/cinesubzdl.js
+++ api/cinesubzdl.js
@@ -6,12 +6,10 @@ module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   
-  // Handle preflight
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
   
-  // Only allow GET method
   if (req.method !== 'GET') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
